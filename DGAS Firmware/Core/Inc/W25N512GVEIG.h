8223a16e9edff7fdcf8a4d51cea6898878defd02
Rhetticle <rhett.humphreys1@gmail.com>
2024-12-31 11:11:11 +1000

    Fix reversed pins on LCD FPC connector

---
+++ DGAS Firmware/Core/Inc/W25N512GVEIG.h
@@ -0,0 +1,14 @@
+/*
+ * W25N512GVEIG.h
+ *
+ *  Created on: 30 Dec. 2024
+ *      Author: rhett
+ */
+
+#ifndef INC_W25N512GVEIG_H_
+#define INC_W25N512GVEIG_H_
+
+#define FLASH_MFR_ID 0xEF
+#define FLASH_DEV_ID 0xAA20
+
+#endif /* INC_W25N512GVEIG_H_ */
